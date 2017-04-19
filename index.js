1592d6b16fb2a18f758361806906edfb533e72a0
refucktor <hmvalcarcel@gmail.com>
2017-04-19 13:48:28 -0400

    Use options to set openpay environment

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const chalk = require('chalk');
-const info = console.info();
+const info = console.info;
 const warn = console.warn;
 const error = console.error;
 
-module.exports = function (lbapp) {
+module.exports = function (lbapp, options) {
     try {
         lbapp.on('started', () => {
-            info(chalk.blue('Loopback component for openpay was enabled.'));
+            process.env.OP_IS_PRODUCTION = !!options.isProduction;
+            info(chalk.blue('\nLoopback component for openpay was enabled.'));
         });
     }
     catch (ex) {
