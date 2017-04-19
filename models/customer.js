317dba08f77ee47d16077d7c5d4cc20b3e566a3f
refucktor <hmvalcarcel@gmail.com>
2017-04-19 14:19:39 -0400

    Fix current issues creating customer

--- models/customer.js
+++ models/customer.js
@@ -4,20 +4,24 @@ const OpenpayClass = require("openpay");
 let openpay = new OpenpayClass(process.env.MERCHANT_ID, process.env.MERCHANT_SK, process.env.OP_IS_PRODUCTION);
 
 module.exports = function (Customer) {
-    Customer.beforeRemote('create', (ctx, modelInstance, next) => {
-        assert(modelInstance.name, "There is no name provided");
-        assert(modelInstance.email, "There is no email provided");
+    Customer.beforeRemote('create', (ctx, unused, next) => {
+        let reqBody = ctx.req.body;
+        if (!reqBody) {
+            next(new Error("Request most have a body with data."));
+        }
+        assert(reqBody.name, "There is no name provided");
+        assert(reqBody.email, "There is no email provided");
 
-        openpay.customer.create(modelInstance, (error, customer) => {
+        openpay.customer.create(reqBody, (error, customer) => {
             if (err) {
-                throw err;
+                next(err);
             }
-            modelInstance.customer_id = customer.id;
-            modelInstance.store = customer.store;
-            modelInstance.creation_date = customer.creation_date;
-            modelInstance.clabe = customer.clabe;
+            reqBody.customer_id = customer.id;
+            reqBody.store = customer.store;
+            reqBody.creation_date = customer.creation_date;
+            reqBody.clabe = customer.clabe;
             next();
-        })
+        });
     });
 
 };
