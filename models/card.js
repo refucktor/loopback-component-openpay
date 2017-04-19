d6e1588f87d914603636c3bed649801dcd0fa152
refucktor <hmvalcarcel@gmail.com>
2017-04-19 14:18:23 -0400

    Fix issues with request

--- models/card.js
+++ models/card.js
@@ -6,28 +6,32 @@ let openpay = new OpenpayClass(process.env.MERCHANT_ID, process.env.MERCHANT_SK,
 
 module.exports = function (Card) {
 
-    Card.beforeRemote('create', (ctx, modelInstance, next) => {
-        assert(modelInstance.card_number, "There is no card_number provided");
-        assert(modelInstance.holder_name, "There is no holder_name provided");
-        assert(modelInstance.expiration_month, "There is no expiration_month provided");
-        assert(modelInstance.expiration_year, "There is no expiration_year provided");
-        assert(modelInstance.cvv2, "There is no cvv2 provided");
+    Card.beforeRemote('create', (ctx, unused, next) => {
+        let reqBody = ctx.req.body;
+        if (!reqBody) {
+            next(new Error("Request most have a body with data."));
+        }
+        assert(reqBody.card_number, "There is no 'card_number' provided");
+        assert(reqBody.holder_name, "There is no 'holder_name' provided");
+        assert(reqBody.expiration_month, "There is no 'expiration_month' provided");
+        assert(reqBody.expiration_year, "There is no 'expiration_year' provided");
+        assert(reqBody.cvv2, "There is no 'cvv2' provided");
 
-        if (!modelInstance.customer_id) {
-            openpay.cards.create(modelInstance, (err, card) => {
+        if (!reqBody.customer_id) {
+            openpay.cards.create(reqBody, (err, card) => {
                 if (err) {
-                    throw err;
+                    next(err);
                 }
-                modelInstance.card_op_id = card.id;
+                reqBody.card_op_id = card.id;
                 next();
             });
         }
         else {
-            openpay.customers.cards.create(modelInstance.customer_id, modelInstance, (err, card) => {
+            openpay.customers.cards.create(reqBody.customer_id, reqBody, (err, card) => {
                 if (err) {
-                    throw err;
+                    next(err);
                 }
-                modelInstance.card_op_id = card.id;
+                reqBody.card_op_id = card.id;
                 next();
             });
         }
